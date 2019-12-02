bf05c6d5e4e43e017c841c06f87bd7a861fbb1ec
Sandeep Sihari <sandeep.sihari@redcarpetup.com>
2019-12-02 15:39:33 -0500

    setting default values to variables

---
+++ variables/variable_default_values.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# If the variables are empty assign them default values
+: ${NAME:="No Name"}
+echo "My name is $NAME"
+
+# Resources http://www.tldp.org/LDP/abs/html/parameter-substitution.html
