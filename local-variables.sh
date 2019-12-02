11ec590933c22bac48a7cd381b38df0298b14454
Sandeep Sihari <sandeep.sihari@redcarpetup.com>
2019-12-02 14:00:07 -0500

    local variable declaration and print to stdout

---
+++ local-variables.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Note: As a rule there should not be any spaces between a variable name, = and its value
+# Local variables exists only within scripts i.e not accessible by other scripts
+
+my_name="Sandeep Sihari"	# declare variable
+echo $my_name			# print value
+unset my_name			# delete variable
