968b2e7a2db76ce887b10a8a71e67798ea0e8f43
Sandeep Sihari <sandeep.sihari@redcarpetup.com>
2019-12-02 14:51:22 -0500

    global variables

--- variables/environment-variables.sh
+++ variables/environment-variables.sh
@@ -8,3 +8,15 @@ export MY_NAME="Hi, I can be accessed by other program or scripts"
 # To access this variable, execute the script in following manner
 # 1 - . environment-variables.sh
 # 2 - source ./environment-variables.sh
+
+# There are lot of global variables in bash.
+# HOME - The current users home directory
+echo "Home Directory $HOME"
+# PATH - A colon(:) seperated list of directories in which shell looks for command
+echo "A colon(:) seperated list of directories in which shell looks for command"
+echo $PATH
+# PWD - The current working directory
+echo "Current working directory $PWD"
+# RANDOM - Random integer between 0 and 32767
+echo "Random integer between 0 and 32767"
+echo $RANDOM
